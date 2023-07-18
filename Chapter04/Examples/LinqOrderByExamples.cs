4e13e6b50a686aa0fe8c859f16d6a00fc5600ef3
Aaron Jared Bagay <aaronbagay@gmail.com>
2023-07-18 15:21:40 -0600

    wrote and examnined some more example code

---
+++ Chapter04/Examples/LinqOrderByExamples.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+
+namespace Chapter04.Examples
+{
+    class LinqOrderByExamples
+    {
+        public static void Main()
+        {
+            var fileInfos = Directory.EnumerateFiles(Path.GetTempPath(),
+            ".tmp").Select(filename => new FileInfo(filename)).ToList();
+            Console.WriteLine("Earlist Files");
+            foreach (var fileInfo in fileInfos.OrderBy(fi => fi.CreationTime))
+            {
+                Console.WriteLine($"{fileInfo.CreationTime:dd MMM yy} : {fileInfo.Name}");
+            }
+
+            Console.WriteLine("Largest Files");
+            foreach (var fileInfo in fileInfos.OrderByDescending(fi => fi.Length))
+            {
+                Console.WriteLine($"{fileInfo.Length:N0} bytes : \t{fileInfo.Name}");
+            }
+            Console.WriteLine("Largest smaller files");
+            foreach (var fileInfo in from fi in fileInfos where fi.Length < 1000 orderby fi.Length descending select fi)
+            {
+                Console.WriteLine($"{fileInfo.Length:N0} bytes \t{fileInfo.Name}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter04/Examples/LinqSelectManyExamples.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Chapter04.Examples
+{
+    record City (String Name, IEnumerable<string> Stations);
+
+    class LinqSelectManyExamples
+    {
+        public static void Main()
+        {
+            var cities = new List<City>
+            {
+                new City ( "London", new[] {"Kings Cross KGX", "Liverpool Street LVS", "Euston EUS"}),
+                new City ( "Birmingham", new[] {"New Street NST"})
+            };
+
+            Console.WriteLine("All Stations: ");
+            foreach (var station in cities.SelectMany(city => city.Stations))
+            {
+                Console.WriteLine(station);
+            }
+        }
+    }
+}
