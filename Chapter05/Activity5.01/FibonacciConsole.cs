232c0dd94374486598140af155ec18907244acfb
Aaron Jared Bagay <aaronbagay@gmail.com>
2023-10-09 15:42:54 -0600

    Added Chapter05/Activity5.01

---
+++ Chapter05/Activity5.01/FibonacciConsole.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter05.Activities.Activity01;
+
+public static class FibonacciConsole
+{
+    public static void Main()
+    {
+        var tempImagePath = Path.GetTempPath() + "Fibonacci\\";
+        if (!Directory.Exists(tempImagePath))
+        {
+            Console.WriteLine($"Creating temp folder: {tempImagePath}");
+            Directory.CreateDirectory(tempImagePath);
+        }
+        else
+        {
+            Console.WriteLine($"Using temp folder: {tempImagePath}");
+        }
+
+        CancellationTokenSource tokenSource = null;
+        do
+        {
+            Console.Write("Phi (eg 1.0 to 6.0) (x=quit, enter=cancel):");
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Cancelling previous tasks");
+                tokenSource?.Cancel();
+                continue;
+            }
+
+            if (input == "x") break;
+
+            if (!double.TryParse(input, NumberStyles.Any, CultureInfo.CurrentCulture, out var phi)) continue;
+
+            Console.Write("Image Count (eg 1000):");
+            input = Console.ReadLine();
+            if (!int.TryParse(input, NumberStyles.Any, CultureInfo.CurrentCulture, out var imageCount)) continue;
+
+            Console.WriteLine($"Creating {imageCount} images...");
+
+            tokenSource = new CancellationTokenSource();
+            tokenSource.Token.Register(() => Console.WriteLine("Cancelled!"));
+
+            var token = tokenSource.Token;
+            Task.Run(() => GenerateImageSequences(tempImagePath, phi, imageCount, token),
+                token);
+        } while (true);
+    }
+
+
+    private static async Task GenerateImageSequences(string tempImagePath, double phi, int imageCount,
+        CancellationToken token)
+    {
+        const double PhiIncrement = 0.015D;
+        const int Points = 3000;
+        const int ImageSize = 800;
+        const int PointSize = 5;
+        ;
+        const string FileExtension = ".png";
+        var fileFormat = ImageFormat.Png;
+
+        for (var i = 0; i < imageCount; i++)
+        {
+            phi += PhiIncrement;
+            var sequence = await Task.Run(() => FibonacciSequence.Calculate(Points, phi), token);
+            if (token.IsCancellationRequested) break;
+
+            var imagePath = $"{tempImagePath}Fibonacci_{Points}_{phi:N3}{FileExtension}";
+
+            await Task.Run(
+                () => ImageGenerator.ExportSequence(sequence, imagePath, fileFormat, ImageSize, ImageSize, PointSize),
+                token);
+        }
+    }
+}
---
+++ Chapter05/Activity5.01/ImageGenerator.cs
@@ -0,0 +1,45 @@
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+
+namespace Chapter05.Activities.Activity01;
+
+public static class ImageGenerator
+{
+    public static void ExportSequence(IList<Fibonacci> sequence,
+        string path, ImageFormat format, int width, int height, double pointSize)
+    {
+        double minX = 0;
+        double maxX = 0;
+        double minY = 0;
+        double maxY = 0;
+
+        foreach (var item in sequence)
+        {
+            if (item.X <= minX) minX = item.X;
+            if (item.X >= maxX) maxX = item.X;
+            if (item.Y <= minY) minY = item.Y;
+            if (item.Y >= maxY) maxY = item.Y;
+        }
+
+        using var image = new Bitmap(width, height);
+
+        using var graphics = Graphics.FromImage(image);
+        graphics.CompositingQuality = CompositingQuality.HighQuality;
+        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        graphics.CompositingMode = CompositingMode.SourceCopy;
+
+        var xRatio = width / (minY - maxY);
+        var yRatio = height / (minX - maxX);
+        foreach (var item in sequence)
+            graphics.FillEllipse(Brushes.Blue,
+                width / 2F + (float)(xRatio * item.X),
+                height / 2F + (float)(yRatio * item.Y),
+                (float)pointSize,
+                (float)pointSize);
+
+        image.Save(path, format);
+        Logger.Log($"Saved {Path.GetFileName(path)}");
+    }
+}
